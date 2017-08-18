46fd40d8af536db7468dea82fbdecdf59b395e2e
Wolfgang <wgehner@gmail.com>
2017-08-18 06:45:13 -0700

    Move transiti to rawgit

--- polymerconference-com/public/_js/main.js
+++ polymerconference-com/public/_js/main.js
@@ -4,17 +4,16 @@ var TM = {
 		console.log('loadLibs called')
 
 		return Promise.all([
-			TS.load('https://cdn.rawgit.com/topseed/topseed-turbo/master/vendor/showup.js')
-			, TS.load('//cdn.rawgit.com/topseed/topseed-turbo/master/webComps/tw0-1.0.js').then(function(){TW.init()}) //Standard Polyfills and Helper
+			TS.load('//cdn.rawgit.com/topseed/topseed-turbo/master/vendor/showup.js')
+			, TS.load('//cdn.rawgit.com/topseed/topseed-turbo/master/webComps/tw-4.1.js').then(function(){TW.init()}) //Standard Polyfills and Helper
 			, TS.load('//cdn.rawgit.com/topseed/topseed-turbo/master/webComps/tp1-1.0.js') //Late-loader Helper for Polymer components
 			, TS.load('/bower_components/webcomponentsjs/HTMLImports.min.js') //Polyfill used in late-loader
 			, TS.load('/bower_components/polymer/polymer.html') //Support for Polymer (1.9.3)
 			, TS.load('/_js/BLX.js') //Support for Message Bus
 			, TS.load('/_js/BDS.js') //Support for Business Data Services (fetch etc)
 			, TS.load('//cdn.jsdelivr.net/jquery.transit/0.9.12/jquery.transit.min.js')
-			, TS.load('/_js/topseed-transitions-1.1.js') //page transitions
-			//, TS.load('https://rawgit.com/topseed/topseed-turbo/master/release/topseed-turbo-4.2.js')
-			, TS.load('/_js/topseed-turbo-4.3.js')
+			, TS.load('//cdn.rawgit.com/topseed/topseed-turbo/master/transit/topseed-transitions-1.1.js') //page transitions
+			, TS.load('//cdn.rawgit.com/topseed/topseed-turbo/master/release/topseed-turbo-4.3.js')
 		])
 		.then(TM.libsLoaded)
 	}
